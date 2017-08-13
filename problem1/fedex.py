75532c60af3baf140f02b88628416860284e42a2
roshankumar6111 <roshankumar6111@outlook.com>
2017-08-13 07:14:56 -0400

    updated

--- problem1/fedex.py
+++ problem1/fedex.py
@@ -1,9 +1,10 @@
 import requests
 import json
 
+#taking tracking number from user
 tracking_number = input("Enter the tracking_number")
 
-
+#featching all the data of tracking number in json format
 data = requests.post('https://www.fedex.com/trackingCal/track', data={
     'data': json.dumps({
         'TrackPackagesRequest': {
@@ -30,7 +31,9 @@ data = requests.post('https://www.fedex.com/trackingCal/track', data={
     'version': 99
 }).json()
 
+#saving particual information in array from json data 
 for printdata in data['TrackPackagesResponse']['packageList']:
     row_array={"tracking no":printdata['trackingNbr'],"ship date":printdata['shipDt'],"status":printdata['keyStatus'],"scheduled delivery":printdata['statusWithDetails']}
 
+#printing data from array in json format
 print (json.dumps(row_array, indent=4))
